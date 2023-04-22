b0100f55198f5eb8b07aa01fd083fde3ff1dd812
joalira00 <joao134.jol@gmail.com>
2023-04-21 22:24:15 -0300

    Adicionada funcionalidade de edição de usuário.

--- Program.cs
+++ Program.cs
@@ -10,19 +10,21 @@
         do
         {
             //Menu Area
-            Console.WriteLine("Boas-vindas!\n1) Para adicionar um novo usuário ao sistema.\n2) Mostrar todos os usuários já cadastrados.\n3) Excluir um usuário do sistema.\n----------------\nX) Sair.");
+            Console.WriteLine("Boas-vindas!\n1) Para adicionar um novo usuário ao sistema.\n2) Mostrar todos os usuários já cadastrados.\n3) Alterar usuário já cadastrado.\n4) Excluir um usuário do sistema.\n---------------------------------\nX) Sair.");
 
             //User's choice verification
             choice = Console.ReadLine();
-            while (choice != "1" && choice != "2" && choice != "3" && choice != "x" && choice != "X")
+            while (choice != "1" && choice != "2" && choice != "3" && choice != "4" && choice != "x" && choice != "X")
             {
                 Console.WriteLine("Opção inválida, por favor escolha novamente.");
                 choice = Console.ReadLine();
+                Console.Clear();
             }
 
-            //Insert name Area
+            //Insert name area
             switch (choice)
             {
+                //Create
                 case "1":
                     string? SignNewUser;
                     do
@@ -50,7 +52,7 @@
                     Console.Clear();
                     break;
 
-
+                //Read
                 case "2":
                     Console.WriteLine("Aqui estão todo os usuários cadastrados:\n");
                     int UserOrder = 0;
@@ -66,7 +68,39 @@
 
                     break;
 
+                //Update
                 case "3":
+                    Console.WriteLine("Abaixo estão todos os usuários cadastrados no sistema:\n");
+                    int UserSort = 0;
+                    foreach (User _user in UserList)
+                    {
+                        Console.WriteLine("{0}.{1}", UserSort, _user.name);
+                        UserSort++;
+                    }
+                    Console.WriteLine("Insira o número do usuário que você deseja alterar:");
+                    int UserEdit = Convert.ToInt32(Console.ReadLine());
+                    while (UserEdit >= UserList.Count)
+                    {
+                        Console.WriteLine("O número escolhido não está de acordo com nenhum usuário no sistema, tente novamente.");
+                        UserEdit = Convert.ToInt32(Console.ReadLine());
+                    }
+
+                    if (UserEdit >= 0 && UserEdit <= UserList.Count)
+                    {
+                        Console.WriteLine("Digite o novo nome de usuário/alteração:");
+                        string? NewUsername = Console.ReadLine();
+
+                        UserList[UserEdit].name = NewUsername;
+                        Console.WriteLine("Alteração feita com sucesso!");
+                    }
+                    Console.WriteLine("\nAperte qualquer tecla para retornar ao menu! :)");
+                    Console.ReadKey();
+                    Console.Clear();
+
+                    break;
+
+                //Delete
+                case "4":
                     Console.WriteLine("Estes são todos os usuários cadastrados no sistema:\n");
                     int UserCounter = 0;
 
@@ -75,6 +109,7 @@
                         Console.WriteLine("{0}.{1}", UserCounter, _user.name);
                         UserCounter++;
                     }
+
                     Console.WriteLine("\nInsira o número do usuário que você deseja excluir:");
                     int UserIndex = Convert.ToInt32(Console.ReadLine());
                     while (UserIndex >= UserList.Count)
@@ -88,11 +123,14 @@
                         UserList.RemoveAt(UserIndex);
                         Console.WriteLine("O usuário removido foi: {0}", RemovedUser.name);
                     }
+
                     Console.WriteLine("\nAperte qualquer tecla para retornar ao menu! :)");
                     Console.ReadKey();
                     Console.Clear();
 
                     break;
+
+                //Close program
                 case "x":
                     Console.WriteLine("Até a próxima! :)");
                     break;
